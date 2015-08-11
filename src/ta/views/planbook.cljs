bb78f57ca5f341d9050799f62a3e9e16c1c7f345
Tom Hutchinson <git@tomisme.com>
2015-08-11 20:33:21 +0800

    reworked reagent components to accept param maps

--- src/ta/views/planbook.cljs
+++ src/ta/views/planbook.cljs
@@ -1,3 +1,6 @@
+;;  refactor complicated components to pass a map of params
+;;  work on activities tab, create activities independently of lessons
+
 (ns ta.views.planbook
   (:require-macros [reagent.ratom :refer [reaction]])
   (:require [ta.views.common :refer [sem e->val icon checkbox dropdown]]
@@ -11,81 +14,42 @@
 (def resource-icon-names {:booklet "book"
                           :worksheet "file"})
 
-(defn lesson-list-item
-  [id {:keys [description subject year finished activity-ids]} selected?]
-  (let [desc-str (if (= description "") "Untitled" description)]
-    [:div {:class (sem "ui" (if selected? "black") "link card")}
-      [:div {:class "content"
-             :onClick #(dispatch [:set-open-lesson id])}
-        [:div {:style {:marginBottom 7}}
-          (if year [:div {:class "ui olive mini label"} (str "Year " year)])
-          (if subject [:div {:class "ui blue mini label"} subject])
-          (if finished [:div {:class "ui green mini label"} "Ready"])]
-        [:div desc-str]]]))
-
-(defn lesson-list
-  [lessons open-lesson]
-  [:div {:class "ui center aligned stacked segment"}
-    [:div {:class "ui header"} "Lesson Stack"]
-    [:div {:class "ui labeled icon button"
-           :onClick #(dispatch [:new-empty-lesson])}
-      (icon "plus") "New Lesson"]
-    (if (not (seq @lessons)) [:div {:class "ui active inline loader"}]
-      [:div {:class "ui items"}
-        (doall
-          (for [lesson @lessons
-                :let [[id content] lesson
-                      selected? (= @open-lesson id)]]
-            ^{:key (str id)} [lesson-list-item id content selected?]))])])
-
 (defn lesson-details
-  [id {:keys [year subject finished description title]}]
-  [:div {:class "ui form" :style {:marginBottom 15}}
-    [:div {:class "right aligned fields"}
+  [{:keys [id lesson]}]
+  (let [{:keys [year subject finished description title]} @lesson]
+    [:div {:class "ui form" :style {:marginBottom 15}}
+      [:div {:class "right aligned fields"}
+        [:div {:class "field"}
+          [:button {:class "ui green icon button"
+                    :on-click #(dispatch [:set-open-lesson nil])}
+              (icon "check")]]
+        [:div {:class "field"}
+          [:button {:class "ui red icon button"
+                    :on-click #(dispatch [:remove-lesson @id])}
+            (icon "trash")]]]
       [:div {:class "field"}
-        [:button {:class "ui green icon button"
-                  :onClick #(dispatch [:set-open-lesson nil])}
-            (icon "check")]]
-      [:div {:class "field"}
-        [:button {:class "ui red icon button"
-                  :onClick #(dispatch [:remove-lesson id])}
-          (icon "trash")]]]
-    [:div {:class "field"}
-      [:input {:type "text"
-               :placeholder "Enter a short description for your lesson"
-               :value description
-               :onChange #(dispatch [:update-lesson
-                                     id :description (e->val %)])}]]
-    [:div {:class "fields"}
-      [:div {:class "field"}
-        [dropdown {:on-change #(dispatch [:update-lesson
-                                          id :year (e->val %)])
-                   :value year
-                   :options year-levels
-                   :starting "Year"}]]
-      [:div {:class "field"}
-        [dropdown {:on-change #(dispatch [:update-lesson
-                                          id :subject (e->val %)])
-                   :value subject
-                   :options subjects
-                   :starting "Subject"}]]
-      [:div {:class "field" :style {:marginTop 5}}
-        [checkbox #(dispatch [:update-lesson id :finished %])
-                  "Ready" finished]]
-      [:div {:class "field"}
-        [:button {:class "ui labeled icon button"}
-         (icon "calendar")
-         "Add to Slot"]]]])
-
-(defn lesson-objectives
-  [id {:keys [objectives]}]
-  [:div
-   [:h4 {:class "ui horizontal divider header"} "Objectives"]
-   [:p "To add lesson objectives, you need to "
-       [:a {:class "ui  horizontal label"
-            :style {:marginLeft 3 :marginTop 3}}
-         "associate this lesson with a unit"]]
-   [:p]])
+        [:input {:type "text"
+                 :placeholder "Enter a short description for your lesson"
+                 :value description
+                 :onChange #(dispatch [:update-lesson @id :description (e->val %)])}]]
+      [:div {:class "fields"}
+        [:div {:class "field"}
+          [dropdown {:on-change #(dispatch [:update-lesson @id :year (e->val %)])
+                     :value year
+                     :options year-levels
+                     :starting "Year"}]]
+        [:div {:class "field"}
+          [dropdown {:on-change #(dispatch [:update-lesson @id :subject (e->val %)])
+                     :value subject
+                     :options subjects
+                     :starting "Subject"}]]
+       [:div {:class "field"}
+         [:button {:class "ui labeled icon disabled button"}
+           (icon "calendar")
+           "Teach"]]
+       [:div {:class "field" :style {:marginTop 5}}
+          [checkbox #(dispatch [:update-lesson @id :finished %])
+                    "Ready" finished]]]]))
 
 (def test-url "readwritethink.org/files/resources/printouts/30697_haiku.pdf")
 
@@ -107,7 +71,7 @@
   [:div {:class "ui fluid card"}
     [:div {:class "content"} description]
     [:div {:class "content"}
-      [:button {:class "ui icon button"
+      [:button {:class "ui icon disabled button"
                 :style {:marginRight 10}}
         [:i {:class "large icons"}
           (icon "file text")
@@ -120,7 +84,7 @@
             description
             (icon "delete icon")])]
     [:div {:class "content"}
-      [:button {:class "ui icon button"
+      [:button {:class "ui icon disabled button"
                 :style {:marginRight 10}}
         [:i {:class "large icons"}
           (icon "tag")
@@ -132,88 +96,121 @@
                    tags)]])
 
 (defn activity-card-list
-  [lesson-id activities]
+  [{:keys [lesson-id activities]}]
     [:div {:class "ui grid"}
-      (for [[id activity] activities
-            :let [length (get activity :length)]]
-        ^{:key (str id)}
-          [:div {:class "row"}
-            [:div {:class "center aligned two wide column"
-                   :style {:paddingRight 0}}
-              [:a {:class "row"}
-                (icon "chevron circle up" :l)]
-              [:div {:class "row"}
-                [:div {:class "ui blue label"}
-                  (str length "m")]]
-              [:a {:class "row"}
-                (icon "chevron circle down" :l)]]
-            [:div {:class "fourteen wide column"}
-              [activity-card [id activity]]]])])
+      (doall
+        (for [[id activity] @activities
+              :let [length (get activity :length)]]
+          ^{:key (str id)}
+            [:div {:class "row"}
+              [:div {:class "center aligned two wide column"
+                     :style {:paddingRight 0}}
+                [:a {:class "row"}
+                  (icon "chevron circle up" :l)]
+                [:div {:class "row"}
+                  [:div {:class "ui blue label"}
+                    (str length "m")]]
+                [:a {:class "row"}
+                  (icon "chevron circle down" :l)]]
+              [:div {:class "fourteen wide column"}
+                [activity-card [id activity]]]]))])
 
 (defn lesson-activities
-  [lesson-id activities]
-  [:div
-    [:h4 {:class "ui horizontal divider header"} "Activities"]
-    [:div {:class "ui grid"}
-      [:div {:class "one column row"}
-        [:div {:class "column"} [activity-card-list lesson-id activities]]]
-      [:div {:class "center aligned one column row"
-             :style {:paddingTop 0}}
-        [:div {:class "column"}
-          [:button {:class "ui labeled icon button"}
-            (icon "cube") "Add an Activity"]]]]])
+  [{:keys [id]}]
+  (let [activities (reaction @(subscribe [:lesson-activities @id]))]
+    [:div
+      [:h4 {:class "ui horizontal divider header"} "Activities"]
+      [:div {:class "ui grid"}
+        [:div {:class "one column row"}
+          [:div {:class "column"} [activity-card-list {:lesson-id id
+                                                       :activities activities}]]]
+        [:div {:class "center aligned one column row"
+               :style {:paddingTop 0}}
+          [:div {:class "column"}
+            [:button {:class "ui labeled icon disabled button"}
+              (icon "cube") "Add an Activity"]]]]]))
 
 (defn lesson-details-panel
-  [lessons id]
-  (let [lesson     (reaction (get @lessons @id))
-        activities (reaction @(subscribe [:lesson-activities @id]))]
-    (fn [_ id]
+  [{:keys [id]}]
+  (let [lesson (reaction @(subscribe [:lesson @id]))]
+    (fn []
       [:div {:class "ui segment"}
-        [lesson-details @id @lesson]
-        [lesson-objectives]
-        [lesson-activities @id @activities]])))
+        [lesson-details {:id id :lesson lesson}]
+        [lesson-activities {:id id}]])))
+
+(defn lesson-list-item
+  [{:keys [id lesson selected?]}]
+  (let [{:keys [description subject year finished activity-ids]} lesson]
+    [:div {:class (sem "ui" (if selected? "black") "link card")}
+      [:div {:class "content"
+             :on-click #(dispatch [:set-open-lesson id])}
+        [:div {:style {:marginBottom 7}}
+          (if year [:div {:class "ui olive mini label"} (str "Year " year)])
+          (if subject [:div {:class "ui blue mini label"} subject])
+          (if finished [:div {:class "ui green mini label"} "Ready"])]
+        [:div (if (= description "")
+                "Untitled"
+                description)]]]))
+
+(defn lesson-list
+  [{:keys [lessons selected]}]
+  [:div {:class "ui center aligned stacked segment"}
+    [:div {:class "ui labeled icon button"
+           :on-click #(dispatch [:new-empty-lesson])}
+      (icon "plus") "Lesson"]
+    (if (not (seq @lessons)) [:div {:class "ui active inline loader"}]
+      [:div {:class "ui items"}
+        (doall
+          (for [lesson @lessons
+                :let [[id content] lesson
+                      selected? (= @selected id)]]
+            ^{:key (str id)}
+              [lesson-list-item {:id id
+                                 :lesson content
+                                 :selected? selected?}]))])])
 
 (defn lessons-tab
-  [lessons open-lesson]
-  [:div {:class "row"}
-    [:div {:class "five wide column"}
-      [lesson-list lessons open-lesson]]
-    [:div {:class "eleven wide column"}
-      (if @open-lesson
-        [lesson-details-panel lessons open-lesson]
-        [:div {:class "circular ui large green inverted label"}
-          (icon "left arrow")
-          "Got time to work on a lesson from your stack?"])]])
+  []
+  (let [lessons     (subscribe [:lessons])
+        open-lesson (subscribe [:open-lesson])]
+    (fn []
+      [:div {:class "row"}
+        [:div {:class "five wide column"}
+          [lesson-list {:lessons lessons :selected open-lesson}]]
+        [:div {:class "eleven wide column"}
+          (if @open-lesson
+            [lesson-details-panel {:id open-lesson}]
+            [:div {:class "circular ui large green inverted label"}
+              (icon "left arrow")
+              "Got time to work on a lesson from your stack?"])]])))
 
 (defn activities-tab
   [activities]
-  [:div
+  [:div {:class "centered row"}
     [:button {:class "ui icon button"
-              :onClick #(dispatch [:new-activity test-activity])}
+              :on-click #(dispatch [:new-activity test-activity])}
       (icon "plus")]
     #_[activity-card-list @activities]])
 
 (defn planbook-view
   []
-  (let [lessons     (subscribe [:lessons])
-        activities  (subscribe [:activities])
-        open-lesson (subscribe [:open-lesson])
-        open-page   (subscribe [:planbook-page])
+  (let [
+        activities (subscribe [:activities])
+        active-tab (subscribe [:planbook-page])
         tabs [{:key :activities :str "Activities" :i "cube"}
               {:key :lessons    :str "Lessons"    :i "file outline"}
               {:key :units      :str "Units"      :i "briefcase"}]]
     (fn []
-      (let [page @open-page]
-        [:div {:class "ui grid"}
-          [:div {:class "centered row"}
-           [:div {:class "two wide colum"}
-             (for [tab tabs :let [{:keys [key str i]} tab]]
-               ^{:key str}
-                 [:button {:onClick #(dispatch [:set-planbook-page key])
-                           :class (sem (if (= page key) "active")
-                                      "ui labeled icon button")}
-                   (icon i) str])]]
-          (condp = page
-            :units      [:p "Lets make some units!"]
-            :lessons    [lessons-tab lessons open-lesson]
-            :activities [activities-tab activities])]))))
+      [:div {:class "ui grid"}
+        [:div {:class "centered one colum row"}
+         [:div {:class "colum"}
+           (doall (for [tab tabs :let [{:keys [key str i]} tab]]
+                    ^{:key str}
+                      [:button {:on-click #(dispatch [:set-planbook-page key])
+                                :class (sem (if (= @active-tab key) "active")
+                                            "ui labeled icon button")}
+                   (icon i) str]))]]
+        (condp = @active-tab
+          :units      [:p "Lets make some units!"]
+          :lessons    [lessons-tab]
+          :activities [activities-tab activities])])))
