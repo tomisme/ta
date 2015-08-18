b6d3fe658208ce1a691cc8ceca02e49b29a5a1db
Tom Hutchinson <git@tomisme.com>
2015-08-18 17:31:10 +0800

    moved routes into core.cljs

--- src/ta/core.cljs
+++ src/ta/core.cljs
@@ -1,14 +1,41 @@
 (ns ta.core
+  (:require-macros [secretary.core :refer [defroute]])
+  (:import goog.History)
   (:require [reagent.core :refer [render]]
-            [re-frame.core :refer [dispatch-sync]]
+            [re-frame.core :refer [dispatch dispatch-sync]]
             [shodan.inspection :refer [inspect]]
+            [secretary.core :as secretary]
+            [goog.events :as events]
+            [goog.history.EventType :as EventType]
+            [ta.views.app :as app]
             [ta.handlers]
-            [ta.subs]
-            [ta.routes :as routes]
-            [ta.views.app :as app]))
+            [ta.subs] ))
 
 (defn init []
   (render [app/app-container] (.getElementById js/document "app")))
 
-(routes/setup-routes)
+(secretary/set-config! :prefix "#")
+
+(defroute "/" []
+  (dispatch [:navigate-to :calendar]))
+
+(defroute "/calendar" []
+  (dispatch [:navigate-to :calendar]))
+
+(defroute "/planbook" []
+  (dispatch [:navigate-to :planbook]))
+
+(defroute "/classes" []
+  (dispatch [:navigate-to :classes]))
+
+(defroute "/calendar/:view/:id" [view id]
+  (dispatch [:view-calendar (case view "day" :day
+                                       "week" :week)
+                            (js/parseInt id)]))
+
+(doto (History.)
+  (events/listen EventType/NAVIGATE
+                 (fn [event] (secretary/dispatch! (.-token event))))
+  (.setEnabled true))
+
 (dispatch-sync [:setup-db])
