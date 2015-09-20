6d2b6bc1318a21a8993e51739b87ac93d6bbe548
Tom Hutchinson <git@tomisme.com>
2015-09-20 21:44:23 +0800

    Start incorporating devcards
    To make the most out of devcards, a lot of components are going to
    need to be rewritten so they are easily testable. This mostly means
    building complicated components out of a single container that sorts
    out handlers and subscriptions, which it then passes to smaller, easily
    tested, static components.

--- src/ta/views/planbook/planbook.cljs
+++ src/ta/views/planbook/planbook.cljs
@@ -1,26 +1,30 @@
 (ns ta.views.planbook.planbook
+  (:require-macros [devcards.core :as dc :refer [defcard deftest]])
   (:require [re-frame.core :as rf]
             [shodan.inspection :refer [inspect]]
-            [ta.views.common :refer [sem icon-el]]
+            [ta.views.common :refer [sem icon-el menu-el]]
             [ta.views.planbook.activities :refer [activities-tab]]
             [ta.views.planbook.lessons :refer [lessons-tab]]))
 
+(def planbook-tabs [{:key :activities :label "Activities" :icon "cubes"}
+                    {:key :lessons    :label "Lessons"    :icon "browser"}
+                    {:key :units      :label "Units"      :icon "briefcase"}])
+
 (defn planbook-view
   []
-  (let [active-tab (rf/subscribe [:open :tab])
-        tabs [{:key :activities :label "Activities" :icon "cubes"}
-              {:key :lessons    :label "Lessons"    :icon "file outline"}
-              {:key :units      :label "Units"      :icon "briefcase"}]]
+  (let [active-tab (rf/subscribe [:open :tab])]
     (fn []
       [:div {:class "ui grid"}
         [:div {:class "centered one colum row"}
-          [:div {:class "ui menu"}
-            (doall (for [tab tabs :let [{:keys [key label icon]} tab]]
-                     ^{:key label}
-                       [:a {:class (sem "item" (if (= @active-tab key) "active"))
-                            :on-click #(rf/dispatch [:set-planbook-open :tab key])}
-                         [:span {:style {:marginRight 5}} (icon-el icon)] label]))]]
+          [menu-el {:items planbook-tabs
+                    :active-items @active-tab
+                    :on-change #(rf/dispatch [:set-planbook-open :tab %])}]]
        (case @active-tab
-          :units      [:p "Lets make some units!"]
-          :lessons    [lessons-tab]
-          :activities [activities-tab])])))
+         :units      [:p "Lets make some units!"]
+         :lessons    [lessons-tab]
+         :activities [activities-tab])])))
+
+(defcard planbook-menu-test
+  (dc/reagent [menu-el {:items planbook-tabs
+                        :active-item :activities
+                        :on-change #(inspect %)}]))
