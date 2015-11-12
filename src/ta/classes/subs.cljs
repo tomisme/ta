9220be7cecee2dc76c359d1bfb7a28ea8558fbd0
Tom Hutchinson <git@tomisme.com>
2015-11-12 18:21:11 +0800

    split classes subs into own file

---
+++ src/ta/classes/subs.cljs
@@ -0,0 +1,26 @@
+(ns ta.classes.subs
+  (:require [re-frame.core :as rf]
+            [ta.util :refer [weekdays]]))
+
+(defn class-in-slot
+  "The id keyword of the first class found for the slot or nil"
+  [classes day session]
+  (some #(if % %) (for [[id class] classes]
+                    (if (= :selected (get-in class [:schedule day session])) id))))
+
+(defn classes->schedule
+  [classes]
+  "A matrix of combined class schedules"
+  (zipmap weekdays (for [day weekdays]
+                     (into [] (for [session (range 5)]
+                                (if (seq classes) (class-in-slot classes day session)))))))
+
+(rf/register-sub
+  :schedule
+  (fn [db _]
+    (reaction (classes->schedule (:classes @db)))))
+
+(rf/register-sub
+  :classes
+  (fn [db _]
+    (reaction (:classes @db))))
--- src/ta/subs.cljs
+++ src/ta/subs.cljs
@@ -1,8 +1,6 @@
 (ns ta.subs
   (:require-macros [reagent.ratom :refer [reaction]])
-  (:require [re-frame.core :as rf]
-            [ta.util :refer [weekdays]]
-            [shodan.inspection :refer [inspect]]))
+  (:require [re-frame.core :as rf]))
 
 (rf/register-sub
   :user
@@ -95,27 +93,3 @@
   :filter
   (fn [db [_ filter]]
     (reaction (get-in @db [:planbook :filters filter]))))
-
-(rf/register-sub
-  :classes
-  (fn [db _]
-    (reaction (:classes @db))))
-
-(defn class-in-slot
-  "The id keyword of the first class found for the slot or nil"
-  [classes day session]
-  (some #(if % %) (for [[id class] classes]
-                    (if (= :selected (get-in class [:schedule day session])) id))))
-
-(defn classes->schedule
-  [classes]
-  "A matrix of combined class schedules"
-  (zipmap weekdays (for [day weekdays]
-                     (into [] (for [session (range 5)]
-                       (if (seq classes)
-                         (class-in-slot classes day session)))))))
-
-(rf/register-sub
-  :schedule
-  (fn [db _]
-    (reaction (classes->schedule (:classes @db)))))
