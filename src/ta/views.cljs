2cf304c333b102a40ada72eff4a168b033ac7905
Tom Hutchinson <git@tomisme.com>
2015-12-01 13:56:28 +0800

    ideas

--- src/ta/views.cljs
+++ src/ta/views.cljs
@@ -2,5 +2,9 @@
   (:require-macros [devcards.core :as dc :refer [defcard defcard-rg]])
   (:require [ta.classes.views]))
 
-(defcard hello
-  "my first card!")
+(defcard "#Views
+  Wherever possible, view/presentation logic should *static* or 'dumb', so it can be used for
+  different purposes/platforms.
+
+  *Idea:* functionality namespaces should only contain static reagent code, containers used to
+  stitch things together could go here?")
